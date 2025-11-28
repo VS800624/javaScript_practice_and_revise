f62a2950e3ac18ad73ef916b8985da5de2ca7d7a
VS800624 <vishalsingh800624@gmail.com>
2025-11-28 15:44:11 +0530

    done  two sum with map

---
+++ leetcode/twoPointers&SlidingWindow/twoSum.js
@@ -0,0 +1,58 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+ 
+
+// Constraints:
+
+// 2 <= nums.length <= 104
+// -109 <= nums[i] <= 109
+// -109 <= target <= 109
+// Only one valid answer exists.
+
+// with brute force
+// var twoSum = function(nums, target) {
+//   for (let i=0; i<nums.length -1 ; i++){
+//     for(let j=i+1; i<nums.length; j++){
+//       if(nums[i] + nums[j] === target){
+//         return [i,j]
+//       }
+//     }
+//   }  
+// };
+
+// with better approach (with map)
+var twoSum = function(nums, target) {
+    let map = {}
+    // storing values  with index in map
+    for(let i=0; i<nums.length ;i++){
+      map[nums[i]] = i
+    }
+// finding the pair
+    for(let i =0; i<nums.length; i++){
+      let pairToFind = target - nums[i] 
+      if(map[pairToFind] && i !== map[pairToFind]){
+        return [i,map[pairToFind]]
+      }
+    }
+};
+
+console.log(twoSum([2,7,11,15],26))
