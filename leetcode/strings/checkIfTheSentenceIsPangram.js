929e5358ed3424fea159805017cb3e5c84f244ad
VS800624 <vishalsingh800624@gmail.com>
2025-12-14 11:21:45 +0530

    optimized check if the sentence is pangram

--- leetcode/strings/checkIfTheSentenceIsPangram.js
+++ leetcode/strings/checkIfTheSentenceIsPangram.js
@@ -20,23 +20,41 @@
 // 1 <= sentence.length <= 1000
 // sentence consists of lowercase English letters.
 
-var checkIfPangram = function(sentence) {
-  let arr = Array(26).fill(0)
-  for(let i = 0; i<26; i++ ){
-    arr[i] =  String.fromCharCode(i + 97)
-  }  
-  for(let i=0; i<arr.length ; i++){
-    if(!sentence.includes(arr[i])){
-      return false
-    }
-  }
-  return true
-  // console.log(arr)
-};
+// var checkIfPangram = function(sentence) {
+//   let arr = Array(26).fill(0)
+//   for(let i = 0; i<26; i++ ){
+//     arr[i] =  String.fromCharCode(i + 97)
+//   }  
+//   for(let i=0; i<arr.length ; i++){
+//     if(!sentence.includes(arr[i])){
+//       return false
+//     }
+//   }
+//   return true
+//   // console.log(arr)
+// };
 
 // Note:
 // char → code → charCodeAt()
 // code → char → String.fromCharCode()
+
+
+// optimize the solution
+// var checkIfPangram = function(sentence) {
+//     let letters = new Set()
+
+//     for(let i =0 ; i<sentence.length ; i++){
+//       letters.add(sentence[i])
+//     }
+//     return letters.size === 26
+// };
+
+// optimizing the solution
+var checkIfPangram = function(sentence) {
+  return new Set(sentence).size === 26  
+};
+
+
 
 
 console.log(checkIfPangram("thequickbrownfoxjumpsoverthelazydog"))
