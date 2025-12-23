a61701df25c0d63cf6714811dc6b07a73ddc0ce1
VS800624 <vishalsingh800624@gmail.com>
2025-12-23 11:08:36 +0530

    Solve sleep

---
+++ leetcode/sleep.js
@@ -0,0 +1,49 @@
+// Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value.
+
+// Note that minor deviation from millis in the actual sleep duration is acceptable.
+
+ 
+
+// Example 1:
+
+// Input: millis = 100
+// Output: 100
+// Explanation: It should return a promise that resolves after 100ms.
+// let t = Date.now();
+// sleep(100).then(() => {
+//   console.log(Date.now() - t); // 100
+// });
+// Example 2:
+
+// Input: millis = 200
+// Output: 200
+// Explanation: It should return a promise that resolves after 200ms.
+
+// async function sleep(millis) {
+//     return new Promise((resolve,reject) => {
+//         setTimeout(() => {
+//             resolve()
+//         }, millis)
+//     })
+// }
+
+async function sleep(millis) {
+    await new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve()
+        }, millis)
+    })
+}
+
+console.log("Start");
+await sleep(2000);
+console.log("End");
+
+
+// sleep() starts
+// ↓
+// Promise returned immediately
+// ↓
+// setTimeout resolves that Promise later
+// ↓
+// await sleep() actually waits
