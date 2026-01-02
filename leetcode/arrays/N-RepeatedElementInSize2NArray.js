f8330f431e4aabb5e94e1d89d7c7e70fde459dd9
VS800624 <vishalsingh800624@gmail.com>
2026-01-02 11:39:39 +0530

    Solve Chunck Array

--- leetcode/arrays/N-RepeatedElementInSize2NArray.js
+++ leetcode/arrays/N-RepeatedElementInSize2NArray.js
@@ -40,14 +40,21 @@
 // };
 
 // with map
+// var repeatedNTimes = function(nums) {
+//     let map = {}
+//     for (let i =0; i<nums.length; i++){
+//       if(map[nums[i]]){
+//         return nums[i]
+//       }
+//       map[nums[i]] = 1
+//     }
+// };
+
 var repeatedNTimes = function(nums) {
-    let map = {}
-    for (let i =0; i<nums.length; i++){
-      if(map[nums[i]]){
-        return nums[i]
-      }
-      map[nums[i]] = 1
+    for(let i =0; i<nums.length; i++){
+      if(nums[i] === nums[i+1] || nums[i] === nums[i+2]) return nums[i]
     }
+    return nums[0]
 };
 
 console.log(repeatedNTimes([5,1,5,2,5,3,5,4]))
