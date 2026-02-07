fc03567664c017f5c9a055c6fda8a70828c827c3
VS800624 <vishalsingh800624@gmail.com>
2026-02-07 10:46:19 +0530

    Optimize Length of Last Word Solution in python

--- python/lengthOfLastWord.py
+++ python/lengthOfLastWord.py
@@ -11,3 +11,12 @@ print(result)
 # word = input()
 # arr = word.strip().split()
 # print(len(arr[-1]))
+
+s = input().strip()
+
+count = 0
+for i in range(len(s)-1,-1,-1):
+    if s[i] == " ":
+        break
+    count += 1
+print(count)
