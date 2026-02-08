141c4805b9c1c7890e3c808b3a1a6d3ece6c3c0e
VS800624 <vishalsingh800624@gmail.com>
2026-02-08 11:36:02 +0530

    Solve Valid Parentheses in python

---
+++ python/validParentheses.py
@@ -0,0 +1,43 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) == 1: return False
+        stack = []
+        for i in range(len(s)):
+            if s[i] == "(" or s[i] == "[" or s[i] == "{":
+                stack.append(s[i])
+            else:
+                if not stack:
+                    return False
+                top = stack.pop()
+                if s[i] == ")" and top != "(" or s[i] == "}" and top != "{" or s[i] == "]" and top!= "[":
+                    return False
+                
+        return len(stack) == 0
+    
+valid = Solution()
+result = valid.isValid("()[]{}")
+print(result)
+
+
+s = input().strip()
+n = len(s)
+if n == 1: print(False)
+valid = True
+
+stack = []
+for ch in s:
+    if ch in "({[":
+        stack.append(ch)
+    else: 
+        if not stack:
+          valid = False
+          break
+        top = stack.pop()
+        if ch == ")" and top != "(" or ch == "}" and top != "{" or ch == "]" and top!= "[":
+          valid = False
+          break
+                
+if valid and len(stack) == 0:
+    print(True)
+else:
+    print(False)
