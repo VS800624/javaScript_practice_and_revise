fcdfc4ce8f32e910888bae5035568e4737db0d9f
VS800624 <vishalsingh800624@gmail.com>
2026-02-15 11:16:59 +0530

    Solve Roman to integer in python with different approach

--- python/romanToInteger.py
+++ python/romanToInteger.py
@@ -23,3 +23,47 @@ class Solution:
 
         return ans
 
+# or  
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        ans = 0
+        roman = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+        
+        for i in range(len(s)-1):
+            if roman[s[i]] < roman[s[i+1]]:
+                ans -= roman[s[i]]
+            else:
+                ans += roman[s[i]]
+
+        return ans + roman[s[-1]]  
+    
+    
+n = int(input())
+s = input()
+
+ans = 0
+roman = {
+  "I": 1,
+  "V": 5,
+  "X": 10,
+  "L": 50,
+  "C": 100,
+  "D": 500,
+  "M": 1000
+}
+
+for i in range(n-1):
+    if roman[s[i]] < roman[s[i+1]]:
+        ans -= roman[s[i]]
+    else:
+        ans += roman[s[i]]
+
+print(ans + roman[s[-1]])
