c0c5ddf50b842f7d36f56f1dae87ab94b56c14f6
VS800624 <vishalsingh800624@gmail.com>
2026-02-16 11:35:52 +0530

    Solve Excel Sheet Column Number

---
+++ DSA_practice/excelSheetColumnNumber.js
@@ -0,0 +1,79 @@
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
+
+// For example:
+
+// A -> 1
+// B -> 2
+// C -> 3
+// ...
+// Z -> 26
+// AA -> 27
+// AB -> 28
+// ...
+
+// Example 1:
+
+// Input: columnTitle = "A"
+// Output: 1
+// Example 2:
+
+// Input: columnTitle = "AB"
+// Output: 28
+// Example 3:
+
+// Input: columnTitle = "ZY"
+// Output: 701
+
+// Constraints:
+
+// 1 <= columnTitle.length <= 7
+// columnTitle consists only of uppercase English letters.
+// columnTitle is in the range ["A", "FXSHRXW"].
+
+var titleToNumber = function (columnTitle) {
+  let map = {
+    A: 1,
+    B: 2,
+    C: 3,
+    D: 4,
+    E: 5,
+    F: 6,
+    G: 7,
+    H: 8,
+    I: 9,
+    J: 10,
+    K: 11,
+    L: 12,
+    M: 13,
+    N: 14,
+    O: 15,
+    P: 16,
+    Q: 17,
+    R: 18,
+    S: 19,
+    T: 20,
+    U: 21,
+    V: 22,
+    W: 23,
+    X: 24,
+    Y: 25,
+    Z: 26,
+  };
+
+  let res = 0;
+
+  let i = 0;
+  let j = columnTitle.length - 1;
+  while (i < columnTitle.length && j >= 0) {
+    res = res + map[columnTitle[i]] * (26 ** j);
+    i++;
+    j--;
+  }
+  return res;
+};
+
+console.log(titleToNumber("ZY"));
