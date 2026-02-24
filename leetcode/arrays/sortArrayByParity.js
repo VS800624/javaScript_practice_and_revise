6ccc1766cd74b205d02e2e93eb81a1ab200b0832
VS800624 <vishalsingh800624@gmail.com>
2026-02-24 10:08:10 +0530

    Solve Sort Array By Parity

---
+++ leetcode/arrays/sortArrayByParity.js
@@ -0,0 +1,45 @@
+// Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+
+// Return any array that satisfies this condition.
+
+// Example 1:
+
+// Input: nums = [3,1,2,4]
+// Output: [2,4,3,1]
+// Explanation: The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+// Example 2:
+
+// Input: nums = [0]
+// Output: [0]
+
+// Constraints:
+
+// 1 <= nums.length <= 5000
+// 0 <= nums[i] <= 5000
+
+var sortArrayByParity = function (nums) {
+  let j = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 0) {
+      temp = nums[i];
+      nums[i] = nums[j];
+      nums[j] = temp;
+      j += 1
+    }
+  }
+  // or
+// let j =0
+// let i =0
+//   while(j<nums.length){
+//     if (nums[j] %2 ===0){
+//       temp = nums[i]
+//       nums[i] = nums[j]
+//       nums[j] = temp
+//       i++
+//     }
+//     j++
+//   }
+  return nums;
+};
+
+console.log(sortArrayByParity([3, 1, 2, 4]));
