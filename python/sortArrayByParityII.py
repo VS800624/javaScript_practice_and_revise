5104ebade854d3e1d575cae67e991fc6ecbd94d1
VS800624 <vishalsingh800624@gmail.com>
2026-02-26 10:12:13 +0530

    Solve sort array by parity II in python

---
+++ python/sortArrayByParityII.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+        i = 0
+        j = 1
+        while i<len(nums) and j<len(nums):
+            if nums[i]%2 == 0:
+                i += 2
+            elif nums[j]%2 !=0:
+                j += 2
+            else:
+                temp = nums[i]
+                nums[i] = nums[j]
+                nums[j] = temp
+
+        return nums
