134e09afed9af7a295e07a098156a31b1bf3befe
VS800624 <vishalsingh800624@gmail.com>
2026-03-01 10:40:53 +0530

    Solve Remove Duplicates from Sorted Array II

--- python/removeDuplicatesFromSortedArray.py
+++ python/removeDuplicatesFromSortedArray.py
@@ -6,8 +6,12 @@ class Solution:
             if nums[i] > nums[k]:
                 k += 1
                 nums[k] = nums[i]
-        print(nums)
+        
         # print(len(set(nums)))
+
+        # for i in range(k+1,len(nums)):
+        #     nums[i] = 0
+        # print(nums)
         return k + 1
 res = Solution()
 ans = res.removeDuplicates([0,0,1,1,1,2,2,3,3,4])
