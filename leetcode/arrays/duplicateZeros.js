74c79828f0acc786334351416d3cc342482d60ab
VS800624 <vishalsingh800624@gmail.com>
2026-03-02 10:10:16 +0530

    Optimize duplicate zeroes solution

--- leetcode/arrays/duplicateZeros.js
+++ leetcode/arrays/duplicateZeros.js
@@ -23,20 +23,52 @@
 // 1 <= arr.length <= 104
 // 0 <= arr[i] <= 9
 
-var duplicateZeros = function(arr) {
+// var duplicateZeros = function(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === 0) {
+      
+//       // Shift elements to the right
+//       for (let j = arr.length - 1; j > i; j--) {
+//         arr[j] = arr[j - 1];
+//       }
+      
+//       // Duplicate zero
+//       i++; // Skip next index
+//     }
+//   }
+  
+//   return arr;
+// };
+
+// more optimize
+var duplicateZeros = function (arr) {
+  let k = 0;
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === 0) {
-      
-      // Shift elements to the right
-      for (let j = arr.length - 1; j > i; j--) {
-        arr[j] = arr[j - 1];
-      }
-      
-      // Duplicate zero
-      i++; // Skip next index
+      k++;
     }
   }
-  
+  // console.log(k)
+  let i = arr.length - 1;
+  let j = arr.length + k - 1;
+  while (i >= 0) {
+    if (arr[i] !== 0) {
+      if (j < arr.length) {
+        arr[j] = arr[i];
+      }
+      j--;
+    } else if (arr[i] === 0) {
+      if (j < arr.length) {
+        arr[j] = 0;
+      }
+      j--;
+      if (j < arr.length) {
+        arr[j] = 0;
+      }
+      j--
+    }
+    i--;
+  }
   return arr;
 };
 
