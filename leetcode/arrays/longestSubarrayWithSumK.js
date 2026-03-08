55f49160b329e704006c64f03c6ab9a026c3ba95
VS800624 <vishalsingh800624@gmail.com>
2026-03-08 10:04:16 +0530

    Solve Longest Subarray with sum K

---
+++ leetcode/arrays/longestSubarrayWithSumK.js
@@ -0,0 +1,69 @@
+// Problem
+
+// Given an integer array nums and an integer k, return the length of the longest subarray whose sum is equal to k.
+
+// If no such subarray exists, return 0.
+
+// A subarray is a contiguous non-empty sequence of elements within an array.
+
+// Example 1
+// Input:
+// nums = [1, 2, 3, 1, 1, 1, 1]
+// k = 3
+
+// Output:
+// 3
+
+// Explanation:
+// The subarrays with sum = 3 are:
+// [1,2], [3], and [1,1,1]
+
+// The longest subarray is [1,1,1] with length = 3.
+// Example 2
+// Input:
+// nums = [1, -1, 5, -2, 3]
+// k = 3
+
+// Output:
+// 4
+
+// Explanation:
+// The subarray [1, -1, 5, -2] has sum = 3 and length = 4.
+// Example 3
+// Input:
+// nums = [-2, -1, 2, 1]
+// k = 1
+
+// Output:
+// 2
+
+// Explanation:
+// The subarray [-1, 2] has sum = 1.
+// Constraints
+
+// 1 <= nums.length <= 10^5
+
+// -10^4 <= nums[i] <= 10^4
+
+// -10^9 <= k <= 10^9
+
+function longestSubarray(arr, k) {
+  let res = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // Sum of subarray from i to j
+    let sum = 0;
+    for (let j = i; j < arr.length; j++) {
+      sum += arr[j];
+      // console.log(sum)
+      // if subarray sum is equal to k
+      if (sum === k) {
+        // find subarray length and update result
+        let subLen = j - i + 1;
+        res = Math.max(res, subLen);
+      }
+    }
+  }
+  return res;
+}
+
+console.log(longestSubarray([1,2,3,1,3,4,5,6,7,1,2,4], 6));
