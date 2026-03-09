9a92dcbce7797882e14ecb54d56fe9f4231003b5
VS800624 <vishalsingh800624@gmail.com>
2026-03-09 10:18:27 +0530

    Optimize longest subarray with sum k solution

--- leetcode/arrays/longestSubarrayWithSumK.js
+++ leetcode/arrays/longestSubarrayWithSumK.js
@@ -47,23 +47,69 @@
 
 // -10^9 <= k <= 10^9
 
+// function longestSubarray(arr, k) {
+//   let res = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     // Sum of subarray from i to j
+//     let sum = 0;
+//     for (let j = i; j < arr.length; j++) {
+//       sum += arr[j];
+//       // console.log(sum)
+//       // if subarray sum is equal to k
+//       if (sum === k) {
+//         // find subarray length and update result
+//         let subLen = j - i + 1;
+//         res = Math.max(res, subLen);
+//       }
+//     }
+//   }
+//   return res;
+// }
+
+// or with hashmap better solution
+// function longestSubarray(arr, k) {
+//   const preSumMap = new Map()
+//   let sum = 0;
+//   let map = {}
+//   let maxLen = 0
+//   for (let i =0; i<arr.length; i++){
+//     sum += arr[i]
+//     // if sum equals to k
+//     if(sum === k){
+//       maxLen = Math.max(maxLen, i+1)
+//     }
+//     let rem  = sum - k
+//     // check if remainder already exists
+//     if (preSumMap.has(rem)) {
+//       let len = i  - preSumMap.get(rem)
+//       maxLen = Math.max(maxLen, len)
+//     } 
+//     // store if prefix sum only first time
+//     if(!preSumMap.has(sum)){
+//       preSumMap.set(sum,i)
+//     }
+//   }
+//   return maxLen;
+// }
+
+// best solution
 function longestSubarray(arr, k) {
-  let res = 0;
-  for (let i = 0; i < arr.length; i++) {
-    // Sum of subarray from i to j
-    let sum = 0;
-    for (let j = i; j < arr.length; j++) {
-      sum += arr[j];
-      // console.log(sum)
-      // if subarray sum is equal to k
-      if (sum === k) {
-        // find subarray length and update result
-        let subLen = j - i + 1;
-        res = Math.max(res, subLen);
-      }
+  let left = right = 0
+  let  sum = arr[0]
+  let maxLen = 0
+  while(right < arr.length){
+    while(left <= right && sum > k){
+      sum -= arr[left]
+      left++
     }
+    if (sum == k){
+      maxLen = Math.max(maxLen, right - left + 1)
+    }
+    right++
+    if (right < arr.length) sum += arr[right]
   }
-  return res;
+  return maxLen;
 }
+
 
 console.log(longestSubarray([1,2,3,1,3,4,5,6,7,1,2,4], 6));
