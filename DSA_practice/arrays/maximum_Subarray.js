52de874794ce355279f991227d2de0d50eed4eaa
VishalSingh619 <vishalsingh800624@gmail.com>
2026-05-17 11:38:11 +0530

    Optimize maximum subarray solution

--- DSA_practice/arrays/maximum_Subarray.js
+++ DSA_practice/arrays/maximum_Subarray.js
@@ -42,6 +42,16 @@
 //     return maxSum
 // };
 
+// optimizing the solution
+var maxSubArray = function(nums) {
+    let maxSum = nums[0]
+    let sum = nums[0]
+    for( let i =1; i<nums.length; i++){
+        sum = Math.max(nums[i], sum + nums[i])
+        maxSum = Math.max(maxSum, sum)
+    }
+    return maxSum
+};
 
 res = maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
 console.log(res);
