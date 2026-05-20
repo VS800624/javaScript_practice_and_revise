745f62a5103268e68b16694d5126fb92316ba09e
VishalSingh619 <vishalsingh800624@gmail.com>
2026-05-20 10:25:21 +0530

    Solve Find the Prefix Common Array of Two Arrays

---
+++ DSA_practice/arrays/Find_the_Prefix_Common_Array_of_Two_Arrays.js
@@ -0,0 +1,32 @@
+var findThePrefixCommonArray = function(A, B) {
+    let freq = {}
+    let count = 0
+    let res = new Array(B.length).fill(0)
+    let i = j = 0
+    for (let i =0; i<A.length; i++){
+        res[i] = count
+        if (!freq[A[i]] ){
+            freq[A[i]] = 1
+        }else{
+            freq[A[i]] += 1
+        }
+
+        if (freq[A[i]] == 2){
+            count++
+       }
+        
+        if (!freq[B[i]]){
+            freq[B[i]] = 1
+        }else{
+            freq[B[i]] += 1
+        }
+       if (freq[B[i]] == 2){
+        count++
+       }
+       res[i] = count
+    }
+    return res
+};
+
+const ans = findThePrefixCommonArray([1,3,2,4],[3,1,2,4])
+console.log(ans);
